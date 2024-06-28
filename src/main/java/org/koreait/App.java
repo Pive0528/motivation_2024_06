2c913b94c1f88e440a29f6203d97ff7cbb6cd7cc
Pive0528 <pive0528@gmail.com>
2024-06-28 11:45:24 +0900

    0628 카페식 정보저장

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,5 +1,9 @@
 package org.koreait;
 
+import org.koreait.motivation.entity.Motivation;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -11,13 +15,17 @@ public class App {
     }
 
     public void run() {
-        System.out.println("== motivation 실행 ==");
+        System.out.println("== motivation execution ==");
+
+        int lastId = 0; // 몇 번까지 썼더라?
+        List<Motivation> motivations = new ArrayList<>(); // motivation 저장소
+
         while (true) {
-            System.out.print("명령어) ");
+            System.out.print("command) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("== motivation 종료 ==");
+                System.out.println("== motivation end ==");
                 break;
             } else if (cmd.length() == 0) {
                 System.out.println("명령어 입력해");
@@ -25,14 +33,38 @@ public class App {
             }
 
             if (cmd.equals("add")) {
-                System.out.print("motivation : ");
-                String motivation = sc.nextLine();
+                int id = lastId + 1;
+                System.out.print("body : ");
+                String body = sc.nextLine();
                 System.out.print("source : ");
                 String source = sc.nextLine();
-                System.out.println("~번 motivation이 등록 되었습니다");
+
+                Motivation motivation = new Motivation(id, body, source);
+
+                motivations.add(motivation);
+
+                System.out.printf("%d번 motivation이 등록 되었습니다\n", id);
+                lastId++; // 마지막 번호 증가
+            } else if (cmd.equals("list")) {
+                if (motivations.size() == 0) {
+                    System.out.println("등록된 motivation 없음");
+                    continue;
+                }
+                System.out.println("== motivation list ==");
+                System.out.printf("  id   //   source   //   body  \n");
+                System.out.println("=".repeat(35));
+
+                for (int i = motivations.size() - 1; i >= 0; i--) {
+                    Motivation motivation = motivations.get(i);
+
+                    if (motivation.getSource().length() > 7) {
+                        System.out.printf("   %d  //    %s    //    %s  \n", motivation.getId(), motivation.getSource().substring(0, 5) + "...", motivation.getBody());
+                        continue;
+                    }
+
+                    System.out.printf("   %d  //    %s     //    %s  \n", motivation.getId(), motivation.getSource(), motivation.getBody());
+                }
             }
         }
-
-
     }
 }
